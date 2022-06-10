5086d4521cab8d11f0f47dffbab7679719105bbf
Alfred Wong <wyfalfred@gmail.com>
2022-06-10 18:54:43 -0500

    enhanced version

--- guess_num_basic.py
+++ guess_num_basic.py
@@ -1,16 +1,20 @@
 import random
 
 r = random.randint(1, 100)
+count = 0
 while True:
-	num = input('please guess a number: ')
+	count += 1 # count = count + 1
+	num = input('please guess a number between 1-100: ')
 	num = int(num)
 	if num == r:
-		print('you are correct! congratulation!')
+		print('Bingo! congratulation!')
+		print('it is your', count, 'guess')
 		break;
 	elif num > r:
-		print('your guess is larger than the actual')
+		print('your guess is larger than the random number')
 	elif num < r:
-		print('your guess is smaller than the actual')
+		print('your guess is smaller than the random number')
+	print('it is your', count, 'guess')
 
 
 
