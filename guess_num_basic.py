ddea0aacd0ebf1c6285f7548970052b29256ef4a
Alfred Wong <wyfalfred@gmail.com>
2022-06-10 19:24:37 -0500

    self enahanced version with fixing end num must larger than start num

--- guess_num_basic.py
+++ guess_num_basic.py
@@ -1,10 +1,20 @@
 import random
+start = input('please input a number as the start of the range for your coming guesses: ')
+end = input('please input a number as the end of the range for your coming guesses: ')
+start = int(start)
+end = int(end)
 
-r = random.randint(1, 100)
+while end < start + 10:
+	print('you cannot input the end number that is smaller than start number, please input a number that is at least 10 larger than your start number ')
+	end = input('please input a number as the end of the range for your coming guesses: ')
+	end = int(end)
+
+r = random.randint(start, end)
 count = 0
+
 while True:
 	count += 1 # count = count + 1
-	num = input('please guess a number between 1-100: ')
+	num = input('please guess a number between your preset range: ')
 	num = int(num)
 	if num == r:
 		print('Bingo! congratulation!')
